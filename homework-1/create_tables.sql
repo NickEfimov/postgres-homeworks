45754c51171b3bcfd3fa8e16904a8313e984fb5f
nikita efimov <nieff777@gmail.com>
2024-02-04 13:47:32 +0400

    first commit

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar (100) NOT NULL,
+	title varchar (100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE costumers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar (100),
+	contact_name varchar (100)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) UNIQUE REFERENCES costumers(customer_id),
+	employee_id int UNIQUE REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar (100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,28 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='12345'
+)
+
+cur = conn.cursor()
+
+f = open('north_data/employees_data.csv', 'r', encoding='utf8')
+data = f.readlines()
+rows = []
+for row in data[1:]:
+    line_tuple = tuple(row.strip(',').split('","'))
+    rows.append(line_tuple)
+    final = (rows[-1:])
+    print(final)
+    #cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", final)
+
+
+#conn.commit()
+
+#cur.close()
+#conn.close()
