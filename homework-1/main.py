d4d70305ee41a24566b1fba0d3d474d8710e747f
nikita efimov <nieff777@gmail.com>
2024-02-04 19:01:15 +0400

    first commit

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,6 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
+import csv
 
 
 conn = psycopg2.connect(
@@ -11,18 +12,25 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-f = open('north_data/employees_data.csv', 'r', encoding='utf8')
-data = f.readlines()
-rows = []
-for row in data[1:]:
-    line_tuple = tuple(row.strip(',').split('","'))
-    rows.append(line_tuple)
-    final = (rows[-1:])
-    print(final)
-    #cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", final)
+#with open('north_data/employees_data.csv', 'r') as f:
+    #reader = csv.reader(f)
+    #next(reader)
+    #for row in reader:
+        #cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute("INSERT INTO costumers VALUES (%s, %s, %s)", row)
+
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
 
 
-#conn.commit()
-
-#cur.close()
-#conn.close()
+conn.commit()
+cur.close()
+conn.close()
