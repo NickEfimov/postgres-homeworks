ae1a42b0d9348f705de2c59981fe60a5257a86d9
nikita efimov <nieff777@gmail.com>
2024-02-05 19:28:00 +0400

    final commit

--- homework-1/main.py
+++ homework-1/main.py
@@ -12,11 +12,11 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-#with open('north_data/employees_data.csv', 'r') as f:
-    #reader = csv.reader(f)
-    #next(reader)
-    #for row in reader:
-        #cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+with open('north_data/employees_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
 
 with open('north_data/customers_data.csv', 'r') as f:
     reader = csv.reader(f)
